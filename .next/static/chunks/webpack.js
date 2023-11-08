e8040c776611c72f9b139d632da96b1e522f6746
AndreyKapus <kapustnikov@ukr.net>
2023-11-08 13:35:16 +0200

    logo style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17b3134c2a6ca57b"; }
+/******/ 		__webpack_require__.h = function() { return "d353ec20afe79432"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/layout.js
+++ Components/layout.js
@@ -5,21 +5,23 @@ import styles from "../styles/layout.module.scss";
 const Layout = ({ children, title = "Portfolio" }) => {
   return (
     <>
-      <nav className={styles.navigation}>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <h1>
-          <Link className={styles.mainTitle} href={"/"}>
-            Portfolio
-          </Link>
-        </h1>
-        <p>
-          <Link className={styles.contactsLink} href={"/Contacts/contacts"}>
-            Contacts
-          </Link>
-        </p>
-      </nav>
+      <div className={styles.container}>
+        <nav className={styles.navigation}>
+          <Head>
+            <title>{title}</title>
+          </Head>
+          <h1>
+            <Link className={styles.mainTitle} href={"/"}>
+              Port<span className={styles.titleAccent}>folio</span>
+            </Link>
+          </h1>
+          <p>
+            <Link className={styles.contactsLink} href={"/Contacts/contacts"}>
+              Contacts
+            </Link>
+          </p>
+        </nav>
+      </div>
       <main>{children}</main>
     </>
   );

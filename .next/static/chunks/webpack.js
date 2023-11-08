b9a83e2ca6baaf527cd0b465daa276d47ded1400
AndreyKapus <kapustnikov@ukr.net>
2023-11-08 14:42:14 +0200

    projects list

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d353ec20afe79432"; }
+/******/ 		__webpack_require__.h = function() { return "b920cdc94209be52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/ProjectsList/ProjectsList.js
+++ pages/ProjectsList/ProjectsList.js
@@ -1,14 +1,15 @@
 import Layout from "../../Components/layout";
+import projects from "../../Components/Projects/projects.json";
 
 const ProjectsList = () => {
   return (
     <div>
       <ul>
         <li>
-          <p>Image</p>
-          <h2>Title</h2>
-          <p>technologies</p>
-          <p>description</p>
+          <p></p>
+          <h2>{projects.title}</h2>
+          <p>{projects.techs}</p>
+          <p>{projects.descr}</p>
         </li>
       </ul>
     </div>

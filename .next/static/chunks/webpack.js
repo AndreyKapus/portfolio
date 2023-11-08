6f813e389f13703877cfdf4c47572f4f1f70388a
AndreyKapus <kapustnikov@ukr.net>
2023-11-08 13:22:01 +0200

    layout styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f94dd7dcd21d85a"; }
+/******/ 		__webpack_require__.h = function() { return "17b3134c2a6ca57b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/layout.js
+++ Components/layout.js
@@ -1,19 +1,24 @@
 import Head from "next/head";
 import Link from "next/link";
+import styles from "../styles/layout.module.scss";
 
 const Layout = ({ children, title = "Portfolio" }) => {
   return (
     <>
-      <nav>
+      <nav className={styles.navigation}>
         <Head>
           <title>{title}</title>
         </Head>
         <h1>
-          <Link href={"/"}>Portfolio</Link>
+          <Link className={styles.mainTitle} href={"/"}>
+            Portfolio
+          </Link>
         </h1>
-        <div>
-          <Link href={"/Contacts/contacts"}>Contacts</Link>
-        </div>
+        <p>
+          <Link className={styles.contactsLink} href={"/Contacts/contacts"}>
+            Contacts
+          </Link>
+        </p>
       </nav>
       <main>{children}</main>
     </>

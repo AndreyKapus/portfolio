049a883ab3b07b336ec11dc662413cd0521053ce
AndreyKapus <kapustnikov@ukr.net>
2024-01-19 12:13:25 +0200

    styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8b89aaf7354e25b6"; }
+/******/ 		__webpack_require__.h = function() { return "2e363873957ff6f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

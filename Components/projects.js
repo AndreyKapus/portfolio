6ab17fc9813eeead0ff979b1fd1815f02d41f31b
AndreyKapus <kapustnikov@ukr.net>
2024-01-20 13:04:38 +0200

    Test

--- Components/projects.js
+++ Components/projects.js
@@ -27,6 +27,7 @@ const Projects = ({ projects }) => {
                 </div>
                 <div>{team ? <p>Team project</p> : <p>Individual</p>}</div>
               </div>
+              <div>ffff</div>
               <DetailsBtn projectId={_id} />
               <DeleteBtn />
             </li>
